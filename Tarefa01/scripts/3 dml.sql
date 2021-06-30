8942d7df6fef619a160a5a1853e2d25c424743a6
Flavio dos Santos <flaviovorthrox@yahoo.com.br>
2021-06-30 13:00:08 -0300

    acrescentando scripts no dml.sql

--- Tarefa01/scripts/3 dml.sql
+++ Tarefa01/scripts/3 dml.sql
@@ -1,11 +1,69 @@
--- 1) Encontre os nomes de todos os empregados que trabalham para a Companhia Soft Sell.
+-- 1) Encontre os nomes de todos os empregados que trabalham 
+--    para a Companhia Soft Sell.
 
--- 2) Encontre todos os nomes das cidades dos empregados que trabalham na Soft Sell.
+SELECT nome_empregado
+FROM trabalha AS t
+INNER JOIN empregado AS e
+ON t.cod_empregado = e.cod_empregado
+INNER JOIN companhia AS c
+ON t.cod_companhia = c.cod_companhia
+WHERE c.nome_companhia = 'Soft Sell'
 
--- 3) Encontre os nomes, endereço e cidade da residência de todos os empregados da Soft Sell que ganham mais de dez  mil reais.
+-- 2) Encontre todos os nomes das cidades dos empregados que 
+--    trabalham na Soft Sell.
 
--- 4) Encontre os nomes de todos os empregados, no banco de dados, que moram na mesma cidade da companhia em que trabalham.
+SELECT e.cidade
+FROM trabalha AS t
+INNER JOIN empregado AS e
+ON t.cod_empregado = e.cod_empregado
+INNER JOIN companhia AS c
+ON t.cod_companhia = c.cod_companhia
+WHERE c.nome_companhia = 'Soft Sell'
 
--- 5) Encontre os nomes de todos os empregados que moram na mesma cidade e rua de seu gerente.
+-- 3) Encontre os nomes, endereço e cidade da residência de 
+--    todos os empregados da Soft Sell que ganham mais de 
+--    dez mil reais.
 
--- 6) Encontre os nomes de todos os empregados, no banco de dados, que não trabalham para a Soft Sell.
+SELECT e.nome_empregado, e.rua, e.cidade
+FROM trabalha t
+INNER JOIN empregado e
+ON t.cod_empregado = e.cod_empregado
+INNER JOIN companhia c
+ON t.cod_companhia = c.cod_companhia
+WHERE c.nome_companhia = 'Soft Sell' 
+	AND e.salario > 10000 
+
+-- 4) Encontre os nomes de todos os empregados, no banco de 
+--    dados, que moram na mesma cidade da companhia em que 
+--    trabalham.
+
+SELECT e.nome_empregado
+FROM trabalha t
+INNER JOIN empregado e
+ON t.cod_empregado = e.cod_empregado
+INNER JOIN companhia c
+ON t.cod_companhia = c.cod_companhia
+WHERE c.cidade_companhia = e.cidade 
+
+-- 5) Encontre os nomes de todos os empregados que moram na 
+--    mesma cidade e rua de seu gerente.
+ ### encontra o nome dos gerentes
+(SELECT e.nome_empregado
+FROM trabalha t
+INNER JOIN empregado e
+ON t.cod_empregado = e.cod_empregado
+INNER JOIN companhia c
+ON t.cod_companhia = c.cod_companhia
+INNER JOIN gerente g
+ON g.cod_empregado = e.cod_empregado)
+
+-- 6) Encontre os nomes de todos os empregados, no banco de 
+--    dados, que não trabalham para a Soft Sell.
+
+SELECT nome_empregado
+FROM trabalha AS t
+INNER JOIN empregado AS e
+ON t.cod_empregado = e.cod_empregado
+INNER JOIN companhia AS c
+ON t.cod_companhia = c.cod_companhia
+WHERE c.nome_companhia != 'Soft Sell'
