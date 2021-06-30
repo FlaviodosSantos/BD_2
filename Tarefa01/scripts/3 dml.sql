54ef467bc17b26cacfc830e91794ad87438aeaa2
Flavio Roberto O. dos Santos <53623916+FlaviodosSantos@users.noreply.github.com>
2021-06-30 08:41:00 -0300

    criando o dml sql

---
+++ Tarefa01/scripts/3 dml.sql
@@ -0,0 +1,11 @@
+-- 1) Encontre os nomes de todos os empregados que trabalham para a Companhia Soft Sell.
+
+-- 2) Encontre todos os nomes das cidades dos empregados que trabalham na Soft Sell.
+
+-- 3) Encontre os nomes, endereço e cidade da residência de todos os empregados da Soft Sell que ganham mais de dez  mil reais.
+
+-- 4) Encontre os nomes de todos os empregados, no banco de dados, que moram na mesma cidade da companhia em que trabalham.
+
+-- 5) Encontre os nomes de todos os empregados que moram na mesma cidade e rua de seu gerente.
+
+-- 6) Encontre os nomes de todos os empregados, no banco de dados, que não trabalham para a Soft Sell.
